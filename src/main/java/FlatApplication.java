0b1d46ba29b14825a816c52f3aec2e01de061fca
yarikhryn <yarikhryn@yandex.ru>
2015-11-27 00:42:55 +0200

    The end

---
+++ src/main/java/FlatApplication.java
@@ -0,0 +1,10 @@
+import org.main.flat.flat.mojo.GitInfo;
+
+
+public class FlatApplication {
+
+    public static void main(String[] args) {
+        System.out.println("TAG : " + GitInfo.TAG);
+    }
+
+}
--- src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by user on 26.11.2015.
- */
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
