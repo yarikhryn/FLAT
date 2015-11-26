c0416f4fa71202b5f8c3848b19028750f4063818
miron222 <tchmunevitch@bigmir.net>
2015-11-27 01:04:28 +0200

    New commit

--- src/main/java/FlatApplication.java
+++ src/main/java/FlatApplication.java
@@ -4,7 +4,10 @@ import org.main.flat.flat.mojo.GitInfo;
 public class FlatApplication {
 
     public static void main(String[] args) {
+
         System.out.println("TAG : " + GitInfo.TAG);
+        System.out.println("I am Vova");
     }
+
 
 }
